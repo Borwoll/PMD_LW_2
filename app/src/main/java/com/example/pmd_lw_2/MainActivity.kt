11b36cadff46333a8daa2e86202677313bf5eb37
Borwoll <github.com/borwoll>
2023-03-15 09:28:31 +0300

    implemented the first activity containing cookbook categories

--- app/src/main/java/com/example/pmd_lw_2/MainActivity.kt
+++ app/src/main/java/com/example/pmd_lw_2/MainActivity.kt
@@ -12,7 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val myListData: Array<ListData> = arrayOf<ListData>(
-            ListData("Блюда из курицы", R.drawable.add_box_foreground)
+            ListData("Первые блюда", R.mipmap.catalog_1_foreground),
+            ListData("Вторые блюда", R.mipmap.catalog_2_foreground),
+            ListData("Салаты", R.mipmap.catalog_6_foreground),
+            ListData("Закуски", R.mipmap.catalog_3_foreground),
+            ListData("Десерты", R.mipmap.catalog_5_foreground),
+            ListData("Выпечка", R.mipmap.catalog_4_foreground),
+            ListData("Напитки", R.mipmap.catalog_9_foreground)
         )
         val recyclerView = findViewById<View>(R.id.recyclerView) as RecyclerView
         val adapter = ListAdapter(myListData)
